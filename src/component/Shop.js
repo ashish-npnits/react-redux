3d84866cc5d791443bdbaf51c1823a7ce7e1c234
Ashish Kumar <ashish@npnits.com>
2023-09-19 11:36:59 +0530

    basic structure

---
+++ src/component/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Shop() {
+  return (
+    <div className='my-4'>
+        <div className='my-4'>Deposit withdraw money</div>
+      <button className='btn btn-primary mx-2'>-</button>
+        Update Balance
+      <button className='btn btn-primary mx-2'>+</button>
+    </div>
+  )
+}
+
+export default Shop
